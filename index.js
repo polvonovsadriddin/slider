e89750a6b292cfd61cb486b7559ff4c914131a4c
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2024-10-19 16:30:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.rightbutton',
+      prevEl: '.leftbutton',
+    },
+});
+const swiper2 = new Swiper('.swiper2', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+      el: '.swiper-pagination2',
+  },
+
+  // Navigation arrows
+  navigation: {
+      nextEl: '.rightbutton2',
+      prevEl: '.leftbutton2',
+  },
+});
+const swiper3 = new Swiper('.swiper3', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+      el: '.swiper-pagination3',
+  },
+
+  // Navigation arrows
+  navigation: {
+      nextEl: '.rigthtbut3',
+      prevEl: '.leftbut3',
+  },
+});
